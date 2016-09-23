4b98dc8615ab7e4505818a04c5b5500e5941997b
Tom Jack <alba4028@o2.pl>
2016-09-23 20:20:52 +0200

    Add post to todos

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-var todos =[{
-	id:1,
-	description:'Meet mom for lunch',
-	completed:false
-},
-{
-	id:2,
-	description:'Gp to market',
-	completed:false
-},
-{
-	id:3,
-	description:"Feed the cat",
-	completed:true
-}];
+// var todos =[{
+	// id:1,
+	// description:'Meet Ben for lunch',
+	// completed:false
+// },
+// {
+	// id:2,
+	// description:'Gp to market',
+	// completed:false
+// },
+// {
+	// id:3,
+	// description:"Feed the cat",
+	// completed:true
+// }];
+var todos = [];
+var todoNextId =1;
 
+
+app.use(bodyParser.json());
 app.get('/',function(req,res){
 	res.send('Todo API Root');
 });
@@ -49,6 +54,24 @@ app.get('/todos/:id',function(req,res){
 	// res.send('Asking for todo with id of '+req.params.id);
 });
 
+//POST /todos
+
+app.post('/todos',function(req,res){
+	var body = req.body;
+		console.log(body);
+	//add id field
+	body.id = todoNextId++;
+
+	//push body into array
+	todos.push(body);
+
+	//console.log('description: '+ body.description);
+
+	res.json(body);
+});
+
 app.listen(PORT,function(){
+	console.log(process.cwd());
+	console.log(process.pid);
 	console.log('Express listening on port: '+PORT+'!');
 });
