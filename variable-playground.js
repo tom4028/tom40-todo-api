1bb59ca1ea9b8eb72019752801cc8b8c04afd111
Tom Jack <alba4028@o2.pl>
2016-09-24 22:40:22 +0200

    Add variable-playground

---
+++ variable-playground.js
@@ -0,0 +1,35 @@
+// var person ={
+// 	name:'Tom',
+// 	age:41
+// }
+
+// function updatepPerson(obj){
+// 	// obj ={
+// 	// 	name:'Tom',
+// 	// 	age:14
+// 	// }
+// 	obj.age = 14;
+// 	obj.description = "My friend";
+// }
+
+// updatepPerson(person);
+// console.log(person);
+
+
+// person.age = 14;
+// console.log(person);
+
+//Array Example
+
+var grades =[15,88];
+
+function addGrades(gradesArr){
+	gradesArr.push(55);
+	debugger;
+	//return gradesArr = [12,88,55];
+	
+}
+
+addGrades(grades);
+
+console.log(grades);
